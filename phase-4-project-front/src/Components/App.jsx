7eff02ff26a6d296dc6c45fb593bd11b29dbed97
JackieCisneros <krieger.jacqueline@gmail.com>
2023-04-14 13:31:49 -0600

    vite app created

---
+++ phase-4-project-front/src/Components/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import '../Styling/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <p>Hello!</p>
+      </div>
+      <input></input><button>Search For Pets</button>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ phase-4-project-front/src/Components/Login.jsx
@@ -0,0 +1,13 @@
+
+
+function Login(){
+    return(
+        <div>
+            <input></input>
+            <button>username</button>
+
+            <input></input>
+            <button>email</button>
+        </div>
+    )
+}

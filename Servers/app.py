ba52265afc27f1395d038218262275b06a7395a6
JackieCisneros <krieger.jacqueline@gmail.com>
2023-04-17 09:11:07 -0600

    added models and app to server

---
+++ Servers/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, make_response, jsonify, session
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db
+
+app = Flask(__name__)
+app.secret_key = #generate a secret key and encrypt it
+#python -c 'import os; print(os.urandom(16))'
+#this needs to be with postgres
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+#some boiler plate code for writing sessions
+@app.route('/sessions/<string:key>', methods=['GET'])
+def show_session(key):
+
+    session["hello"] = session.get("hello") or "World"
+    session["goodnight"] = session.get("goodnight") or "Moon"
+
+    response = make_response(jsonify({
+        'session': {
+            'session_key': key,
+            'session_value': session[key],
+            'session_accessed': session.accessed,
+        },
+        'cookies': [{cookie: request.cookies[cookie]}
+            for cookie in request.cookies],
+    }), 200)
+
+    response.set_cookie('mouse', 'Cookie')
+
+    return response

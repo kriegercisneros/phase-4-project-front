a1ae733b0a626b102e98daa5d9a0820df0ffcdfe
JackieCisneros <krieger.jacqueline@gmail.com>
2023-04-18 10:43:11 -0600

    merged

---
+++ Client/src/Components/Signup.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import {Formik} from 'formik'
+
+export const SignupForm = () =>{
+    const [users, setUsers] = useState([{}]);
+    const [refreshPage, setRefreshPage] = useState(false);
+
+    useEffect(() => {
+        console.log("fetching users...")
+        fetch('/users')
+    })
+} 
+
+    const formik = useFormik({
+        initialValues:{
+            company_name:'',
+            email:'',
+            location:'',
+            password:'',
+            re_enter_password:''
+        },
+        validationSchema: formSchema,
+    onSubmit:(values)=>{
+        fetch("customers", {
+
+        })
+    }
+    })
